69d0b2ea33aa3b781c07524197ea7d6af915d693
Rayhan Nasrulloh <raihan.rosidi354n@gmail.com>
2025-07-01 11:50:51 +0700

    Enhance UI with hover effects on service cards and integrate AOS for animations across multiple pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
 const navbar = document.querySelector('.navbar');
-
 hamburger.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
 
+
+AOS.init();
+
+new TypeIt("#hero-title", {
+    speed: 75,
+    waitUntilVisible: true,
+}).go();
